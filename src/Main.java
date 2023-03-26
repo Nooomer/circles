deaa1378e2325d4a89ade86ecc741de623a7b5ba
Nooomer <ampilogov.qaz@gmail.com>
2023-03-26 14:17:27 +0500

    New example

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import shapes.Circles;
+
 public class Main {
     public static void main(String[] args) {
         try {
             Circles circles1 = new Circles(0, 0, 2);
             Circles circles2 = new Circles(1, 1, 2);
-            Circles circles3 = new Circles(1, 1, -1);
+            //Circles circles3 = new Circles(1, 1, -1);
             Circles randomCircles = new Circles();
+            int x = circles1.getDots().getCoordX();
             if (Circles.haveShareDots(circles1, circles2)) {
                 System.out.print("Circles have share dots");
             } else {
                 System.out.print("Circles haven't share dots");
             }
         } catch (Exception e) {
-            System.out.println("Circles radius less 1!!");
+            System.out.println("shapes.Circles radius less 1!!");
         }
     }
 }
